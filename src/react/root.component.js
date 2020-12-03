bdaf4ef08afc041d50ddd42ddd0c86f06f77831f
annatara42 <annatara42@gmail.com>
2020-12-03 01:12:35 -0500

    Initial commit

---
+++ src/react/root.component.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const App = () => <h1>Calling from React Component</h1>
+
+export default App
---
+++ src/vue/main.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+      <h1>Calling from Vue Component</h1>
+  </div>
+</template>
